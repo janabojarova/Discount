eb7dbbb98adf77b202df864d61e1b1a7d2d97a4a
Jana Bojarova <thejana13@gmail.com>
2022-05-31 22:08:36 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace discountt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sumPurchase;
+            Console.WriteLine("Enter the amount of purchase and we teel you your discount for next time:");
+            sumPurchase = Convert.ToInt32(Console.ReadLine());
+
+            if (sumPurchase < 10)
+            {
+                Console.WriteLine(" Minimum the amount of purchase for discount is 10 EURO!!!");
+
+            }
+            else if (sumPurchase >= 10 && sumPurchase <= 20)
+            {
+                Console.WriteLine(" Your discount for next purchase is 1 EURO!");
+
+            }
+            else if (sumPurchase >= 20 && sumPurchase <= 50)
+            {
+                Console.WriteLine("Your discount for next purchase is 5 EURO!");
+            }
+            else if (sumPurchase > 50 && sumPurchase <= 100)
+            {
+                Console.WriteLine("Your discount for next purchase is 10 EURO!");
+            }
+
+
+            else
+            {
+                Console.WriteLine("Your discount for next purchase is 15 EURO!");
+            }
+            Console.WriteLine("Have a nice day!");
+        }
+    }
+}
